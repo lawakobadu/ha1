70c754f181dc0d36f55c8e685fd546e43a17ec94
ha3 <fadliramadhan1707@gmail.com>
2024-08-30 08:05:57 +0000

    fix

--- app.py
+++ app.py
@@ -1,43 +1,103 @@
 from flask import Flask, jsonify, render_template, Response, request, url_for, session, redirect, flash
-import subprocess, re
+from werkzeug.security import generate_password_hash, check_password_hash
+import subprocess, re, os
 
 app = Flask(__name__)
 app.secret_key = 'haproxy'
 
-USERNAME = "admin"
-PASSWORD = "admin123"
+users = {
+    'admin': generate_password_hash('admin123')
+}
 
 # Path ke file konfigurasi HAProxy di dalam folder static
 CONFIG_FILE_PATH = ('/etc/haproxy/haproxy.cfg')
+SSL_FILE_PATH = ('/etc/haproxy-dashboard/ssl/key.pem')
+DOMAIN_FILE_PATH = ('/etc/haproxy-dashboard/ssl/domain.txt')
+
+
+def certificate(content):
+    with open(SSL_FILE_PATH, 'w') as file:
+        file.write(content)
+    return f"{SSL_FILE_PATH}"
+
+def domain(domain_content):
+    with open(DOMAIN_FILE_PATH, 'w') as file:
+        file.write(domain_content)
+    return f"{DOMAIN_FILE_PATH}"
+
 
 def is_haproxy_exist():
-    with open('/etc/haproxy/haproxy.cfg', 'r') as haproxy_cfg:
+    with open(CONFIG_FILE_PATH, 'r') as haproxy_cfg:
         content = haproxy_cfg.read()
         if "frontend " in content or "backend " in content:
             return True
     return False
+
+def is_domain_exist():
+    if os.path.exists(DOMAIN_FILE_PATH):
+        with open(DOMAIN_FILE_PATH, 'r') as domain_file:
+            content = domain_file.read()
+        return content  
+    else:
+        return None  
+
+
 
 def read_from_file():
     data = []
     with open(CONFIG_FILE_PATH, 'r') as f:
         content = f.read().strip()
 
-        # Memperbarui pola regex untuk menangkap semua elemen dengan benar
-        pattern = re.compile(r'frontend\s+(\S+)\s+.*?bind\s+(\S+):(\d+).*?\n\s+mode\s+(\S+).*?\n\s+default_backend\s+(\S+).*?\nbackend\s+(\S+).*?\n\s+balance\s+(\S+).*?\n\s+server\s+(\S+)\s+(\S+):(\d+)\s+check', re.DOTALL)
-        matches = pattern.findall(content)
-        print("matches : ", matches)
+        # Pola regex untuk frontend dan backend
+        pattern1 = re.compile(r'frontend\s+(\S+)\s+.*?bind\s+(\S+):(\d+).*?\n\s+mode\s+(\S+).*?\n\s+default_backend\s+(\S+).*?\nbackend\s+(\S+).*?\n\s+balance\s+(\S+)', re.DOTALL)
+        frontend_matches = pattern1.findall(content)
 
-        # Format data untuk ditampilkan dalam tabel
-        for match in matches:
+        # Pola regex untuk backend dengan weight
+        pattern2 = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+weight\s+(\d+)\s+check', re.DOTALL)
+        weight_matches = pattern2.findall(content)
+
+        # Pola regex untuk backend tanpa weight
+        pattern3 = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+check', re.DOTALL)
+        server_matches = pattern3.findall(content)
+
+        # print("frontend_matches:", frontend_matches)
+        # print("weight_matches:", weight_matches)
+        # print("server_matches:", server_matches)
+        backend_servers = []
+        
+        for match in frontend_matches:
             haproxy_name = match[0].strip()
             frontend_port = match[2].strip()
             protocol = match[3].strip()
             lb_method = match[6].strip()
-            backend_server_names = match[7].strip()
-            backend_server_ips = match[8].strip()
-            backend_server_ports = match[9].strip()
-            data.append({'haproxy_name': haproxy_name, 'frontend_port': frontend_port, 'protocol': protocol, 'lb_method': lb_method, 'backend_server_names': backend_server_names, 'backend_server_ips': backend_server_ips, 'backend_server_ports': backend_server_ports})
 
+            data.append({
+                'haproxy_name': haproxy_name, 
+                'frontend_port': frontend_port, 
+                'protocol': protocol, 
+                'lb_method': lb_method, 
+                'count_server': len(backend_servers)
+            })
+        
+        if lb_method == 'roundrobin':
+            for weight_match in weight_matches:
+                backend_servers.append({
+                    'name': weight_match[0].strip(),
+                    'ip': weight_match[1].split(':')[0].strip(),
+                    'port': weight_match[1].split(':')[1].strip(),
+                    'weight': weight_match[2].strip()
+                })
+        else:
+            for server_match in server_matches:
+                backend_servers.append({
+                    'name': server_match[0].strip(),
+                    'ip': server_match[1].split(':')[0].strip(),
+                    'port': server_match[1].split(':')[1].strip(),
+                })
+
+        print(backend_servers)
+        data[0]['count_server'] = len(backend_servers)
+        data[0]['backend_servers'] = zip(backend_servers, range(1, len(backend_servers) +1 ))
     return data
 
 def save_to_file(data):
@@ -51,26 +111,35 @@ def save_to_file(data):
     backend_servers = data.get('backend_servers', [])
     use_ssl = data.get('use_ssl', False)
     ssl_cert_path = data.get('ssl_cert_path', '')
+    domain_name = data.get('domain_name', '')
 
+    # Write SSL certificate to file if provided
+    if use_ssl and ssl_cert_path:
+        with open(SSL_FILE_PATH, 'w') as cert_file:
+            cert_file.write(ssl_cert_path)
+        with open(DOMAIN_FILE_PATH, 'w') as domain_file:
+            domain_file.write(domain_name)
+
+    # Write HAProxy configuration
     with open(CONFIG_FILE_PATH, 'a') as haproxy_cfg:
         # Frontend configuration
         haproxy_cfg.write(f"\nfrontend {haproxy_name}\n")
 
         # Bind and protocol configurations
-        if protocol == 'http':
+        if protocol == 'tcp':
             if use_ssl:
-                haproxy_cfg.write(f"        bind *:443 ssl crt {ssl_cert_path}\n")
-                haproxy_cfg.write(f"        redirect scheme https code 301 if !{{ ssl_fc }}\n")
-            else:
-                haproxy_cfg.write(f"        bind *:80\n")
-            haproxy_cfg.write(f"        mode http\n")
-        elif protocol == 'tcp':
-            if use_ssl:
-                haproxy_cfg.write(f"        bind *:{frontend_port} ssl crt {ssl_cert_path}\n")
+                haproxy_cfg.write(f"        bind *:{frontend_port} ssl crt /etc/haproxy-dashboard/ssl/key.pem\n")
                 haproxy_cfg.write(f"        redirect scheme https code 301 if !{{ ssl_fc }}\n")
             else:
                 haproxy_cfg.write(f"        bind *:{frontend_port}\n")
             haproxy_cfg.write(f"        mode tcp\n")
+        elif protocol == 'http':
+            if use_ssl:
+                haproxy_cfg.write(f"        bind *:443 ssl crt /etc/haproxy-dashboard/ssl/key.pem\n")
+                haproxy_cfg.write(f"        redirect scheme https code 301 if !{{ ssl_fc }}\n")
+            else:
+                haproxy_cfg.write(f"        bind *:80\n")
+            haproxy_cfg.write(f"        mode http\n")
 
         haproxy_cfg.write(f"        default_backend {haproxy_name}\n")
 
@@ -84,7 +153,7 @@ def save_to_file(data):
                 backend_server_name, backend_server_ip, backend_server_port, backend_server_weight = backend_server_info
                 if backend_server_name and backend_server_ip and backend_server_port and backend_server_weight:
                     haproxy_cfg.write(f"        server {backend_server_name} {backend_server_ip}:{backend_server_port} weight {backend_server_weight} check\n")
-        elif lb_method != 'roundrobin':
+        else:
             for backend_server_info in backend_servers:
                 backend_server_name, backend_server_ip, backend_server_port = backend_server_info
                 if backend_server_name and backend_server_ip and backend_server_port:
@@ -98,7 +167,6 @@ def save_to_file(data):
         haproxy_cfg.write(f"        stats hide-version\n")
         haproxy_cfg.write(f"        stats uri /stats\n")
         haproxy_cfg.write(f"        stats realm Haproxy\\ Statistics\n")
-        haproxy_cfg.write(f"        stats auth admin:password\n")
 
     return {'success': True, 'message': "Frontend and Backend added successfully."}
 
@@ -226,8 +294,7 @@ def index():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        if username == USERNAME and password == PASSWORD:
-
+        if username in users and check_password_hash(users[username], password):
             session['logged_in'] = True
             session['username'] = username
             return redirect(url_for("home"))  
@@ -244,8 +311,9 @@ def home():
 
     frontend_port, layer7_count, layer4_count, haproxy_name, modes, lb_method, backend_servers = haproxy_info()
     haproxy_exists = is_haproxy_exist()
+    domain_exists = is_domain_exist()
 
-    return render_template('home.html', frontend_port=frontend_port, layer7_count=layer7_count, layer4_count=layer4_count, haproxy_name=haproxy_name, modes=modes, lb_method=lb_method, backend_servers=backend_servers, haproxy_exists=haproxy_exists)
+    return render_template('home.html', frontend_port=frontend_port, layer7_count=layer7_count, layer4_count=layer4_count, haproxy_name=haproxy_name, modes=modes, lb_method=lb_method, backend_servers=backend_servers, haproxy_exists=haproxy_exists, domain_exists=domain_exists)
 
 @app.route('/logout')
 def logout():
@@ -267,6 +335,7 @@ def add():
             'protocol': request.form['protocol'],
             'use_ssl': 'ssl_checkbox' in request.form,
             'ssl_cert_path': request.form.get('ssl_cert_path', ''),
+            'domain_name': request.form.get('domain_name', '')
         }
 
         if lb_method == 'roundrobin':
@@ -314,27 +383,44 @@ def add():
 def edit(haproxy_name):
     if not session.get('logged_in'):
         return '<script>alert("Harus login dulu"); window.location.href = "/";</script>'
-        
+    
+    ssl_cert_content = ''
+    domain_name_content = ''
+
     data = read_from_file()
     item = next((d for d in data if d['haproxy_name'] == haproxy_name), None)
 
     if request.method == 'POST':
         if item:
             # Step 1: Get data from the form
+            ssl_cert_content = request.form.get('ssl_cert_path', '')
+            domain_name_content = request.form.get('domain_name', '')
+            certificate(ssl_cert_content)
+            domain(domain_name_content)
             item.update({
                 'haproxy_name': request.form['haproxy_name'],
                 'frontend_port': request.form['frontend_port'],
                 'protocol': request.form['protocol'],
-                'lb_method': request.form['lb_method'],
                 'use_ssl': 'ssl_checkbox' in request.form,
-                'ssl_cert_path': request.form.get('ssl_cert_path', ''),
-                'backend_servers': list(zip(
+                'ssl_cert_path': ssl_cert_content,
+                'domain_name': domain_name_content,
+                'lb_method': request.form['lb_method']
+            })
+
+            if item['lb_method'] == 'roundrobin':
+                item['backend_servers'] = list(zip(
+                request.form.getlist('backend_server_names'),
+                request.form.getlist('backend_server_ips'),
+                request.form.getlist('backend_server_ports'),
+                request.form.getlist('backend_server_weights')
+                ))
+            else:
+                item['backend_servers'] = list(zip(
                 request.form.getlist('backend_server_names'),
                 request.form.getlist('backend_server_ips'),
                 request.form.getlist('backend_server_ports')
-                ))
-            })
-
+            ))
+        
             # Step 2: Delete the old configuration
             delete_to_file(haproxy_name)
 
@@ -356,7 +442,17 @@ def edit(haproxy_name):
 
             return redirect(url_for('home'))
 
-    return render_template('HAProxy/edit.html', **item)
+    ssl_cert_content = ''
+    if os.path.exists(SSL_FILE_PATH):
+        with open(SSL_FILE_PATH, 'r') as file:
+            ssl_cert_content = file.read()
+    
+    domain_name_content = ''
+    if os.path.exists(DOMAIN_FILE_PATH):
+        with open(DOMAIN_FILE_PATH, 'r') as file:
+            domain_name_content = file.read()
+            
+    return render_template('HAProxy/edit.html', **item, ssl_cert_path=ssl_cert_content, domain_name=domain_name_content)
 
 
 @app.route("/delete", methods=['POST'])
@@ -388,5 +484,25 @@ def delete():
     return render_template('home.html')
 
 
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_password():
+    if not session.get('logged_in'):
+        return '<script>alert("Harus login dulu"); window.location.href = "/";</script>'
+    
+    if request.method == 'POST':
+        new_password = request.form.get('new_password')
+        password_confirmation = request.form.get('password_confirmation')
+        username = session.get('username')
+
+        if new_password == password_confirmation:
+            users[username] = generate_password_hash(new_password)
+            session.clear()
+            return '<script>alert("Password berhasil direset"); window.location.href = "/";</script>'
+        else:
+            return '<script>alert("Password gagal direset"); window.location.href = "/home";</script>'
+    
+    return render_template('reset_password.html')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
