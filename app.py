5c67fc598210a7a5f7b05bac601cadd84cc0c0e7
ha3 <fadliramadhan1707@gmail.com>
2024-09-17 06:55:33 +0000

    fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, Response, request, url_for, session, redirect, flash, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
 import subprocess, re, os, jwt, datetime, json
 from functools import wraps
 
@@ -70,67 +71,81 @@ def is_domain_exist():
         return None  
 
 
-def read_from_file():
+def read_from_file(haproxy_names):
     data = []
     with open(CONFIG_FILE_PATH, 'r') as f:
         content = f.read().strip()
 
-        # Pola regex untuk frontend dan backend
-        pattern1 = re.compile(r'frontend\s+(\S+)\s+.*?bind\s+(\S+):(\d+).*?\n\s+mode\s+(\S+).*?\n\s+default_backend\s+(\S+).*?\nbackend\s+(\S+).*?\n\s+balance\s+(\S+)', re.DOTALL)
-        frontend_matches = pattern1.findall(content)
+        # Pola regex untuk frontend
+        pattern_frontend = re.compile(r'frontend\s+(\S+)\s+.*?bind\s+(\S+):(\d+).*?\n\s+mode\s+(\S+).*?\n\s+default_backend\s+(\S+)', re.DOTALL)
+        frontend_matches = pattern_frontend.findall(content)
 
-        # Pola regex untuk backend dengan weight
-        pattern2 = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+weight\s+(\d+)\s+check', re.DOTALL)
-        weight_matches = pattern2.findall(content)
+        # Pola regex untuk backend
+        pattern_backend = re.compile(r'backend\s+(\S+)\s+balance\s+(\S+)(.*?)(?=\nfrontend|\Z)', re.DOTALL)
+        backend_matches = pattern_backend.findall(content)
 
-        # Pola regex untuk backend tanpa weight
-        pattern3 = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+check', re.DOTALL)
-        server_matches = pattern3.findall(content)
+        # Memetakan backend ke server-servernya
+        pattern_backend_servers_weight = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+weight\s+(\d+)\s+check')
+        pattern_backend_servers_no_weight = re.compile(r'server\s+(\w+)\s+([\d.]+:\d+)\s+check')
 
-        # print("frontend_matches:", frontend_matches)
-        # print("weight_matches:", weight_matches)
-        # print("server_matches:", server_matches)
-        backend_servers = []
-        
+        print("frontend_matches:", frontend_matches)
+        print("backend_matches:", backend_matches)
+
         for match in frontend_matches:
             haproxy_name = match[0].strip()
-            frontend_port = match[2].strip()
-            protocol = match[3].strip()
-            lb_method = match[6].strip()
+            if haproxy_name == haproxy_names:
+                frontend_port = match[2].strip()
+                protocol = match[3].strip()
+                backend_name = match[4].strip()
 
-            data.append({
-                'haproxy_name': haproxy_name, 
-                'frontend_port': frontend_port, 
-                'protocol': protocol, 
-                'lb_method': lb_method, 
-                'count_server': len(backend_servers)
-            })
-        
-        if lb_method == 'roundrobin':
-            for weight_match in weight_matches:
-                backend_servers.append({
-                    'name': weight_match[0].strip(),
-                    'ip': weight_match[1].split(':')[0].strip(),
-                    'port': weight_match[1].split(':')[1].strip(),
-                    'weight': weight_match[2].strip()
-                })
-        else:
-            for server_match in server_matches:
-                backend_servers.append({
-                    'name': server_match[0].strip(),
-                    'ip': server_match[1].split(':')[0].strip(),
-                    'port': server_match[1].split(':')[1].strip(),
-                })
+                # Cari backend yang sesuai dengan default_backend dari frontend
+                backend_data = next(
+                    (b for b in backend_matches if b[0] == backend_name), None
+                )
 
-        print(backend_servers)
-        data[0]['count_server'] = len(backend_servers)
-        data[0]['backend_servers'] = zip(backend_servers, range(1, len(backend_servers) +1 ))
+                if backend_data:
+                    lb_method = backend_data[1].strip()
+                    backend_servers_block = backend_data[2].strip()
+
+                    # Temukan server-server dalam backend
+                    backend_servers = []
+                    
+                    if lb_method == 'roundrobin':
+                        # Jika roundrobin, server-server dengan weight
+                        server_matches = pattern_backend_servers_weight.findall(backend_servers_block)
+                        for server in server_matches:
+                            backend_servers.append({
+                                'name': server[0].strip(),
+                                'ip': server[1].split(':')[0].strip(),
+                                'port': server[1].split(':')[1].strip(),
+                                'weight': server[2].strip()
+                            })
+                    else:
+                        # Jika bukan roundrobin, ambil server tanpa weight
+                        server_matches = pattern_backend_servers_no_weight.findall(backend_servers_block)
+                        for server in server_matches:
+                            backend_servers.append({
+                                'name': server[0].strip(),
+                                'ip': server[1].split(':')[0].strip(),
+                                'port': server[1].split(':')[1].strip(),
+                            })
+
+                    # Append data frontend dengan backend_servers
+                    data.append({
+                        'haproxy_name': haproxy_name, 
+                        'frontend_port': frontend_port, 
+                        'protocol': protocol, 
+                        'lb_method': lb_method, 
+                        'count_server': len(backend_servers),
+                        'backend_servers': list(zip(backend_servers, range(1, len(backend_servers) + 1)))
+                    })
+
+                    print(f"Data for {haproxy_name}: {data[-1]}")
+
     return data
 
-def save_to_file(data):
-    if is_haproxy_exist():
-        return {'success': False, 'message': "Konfigurasi HAProxy sudah ada. Tidak bisa menambah konfigurasi baru."}
 
+def save_to_file(data):
     haproxy_name = data.get('haproxy_name')
     frontend_port = data.get('frontend_port')
     protocol = data.get('protocol')
@@ -149,6 +164,20 @@ def save_to_file(data):
 
     # Write HAProxy configuration
     with open(CONFIG_FILE_PATH, 'a') as haproxy_cfg:
+        # Read current configuration to check for 'listen stats'
+        with open(CONFIG_FILE_PATH, 'r') as haproxy_cfg_read:
+            existing_config = haproxy_cfg_read.read()
+
+        # Stats configuration
+        if 'listen stats' not in existing_config:
+            haproxy_cfg.write(f"\nlisten stats\n")
+            haproxy_cfg.write(f"        bind :9999\n")
+            haproxy_cfg.write(f"        mode http\n")
+            haproxy_cfg.write(f"        stats enable\n")
+            haproxy_cfg.write(f"        stats hide-version\n")
+            haproxy_cfg.write(f"        stats uri /stats\n")
+            haproxy_cfg.write(f"        stats realm Haproxy\\ Statistics\n")
+            
         # Frontend configuration
         haproxy_cfg.write(f"\nfrontend {haproxy_name}\n")
 
@@ -186,15 +215,6 @@ def save_to_file(data):
                 if backend_server_name and backend_server_ip and backend_server_port:
                     haproxy_cfg.write(f"        server {backend_server_name} {backend_server_ip}:{backend_server_port} check\n")
 
-        # Stats configuration
-        haproxy_cfg.write(f"\nlisten stats\n")
-        haproxy_cfg.write(f"        bind :9999\n")
-        haproxy_cfg.write(f"        mode http\n")
-        haproxy_cfg.write(f"        stats enable\n")
-        haproxy_cfg.write(f"        stats hide-version\n")
-        haproxy_cfg.write(f"        stats uri /stats\n")
-        haproxy_cfg.write(f"        stats realm Haproxy\\ Statistics\n")
-
     return {'success': True, 'message': "Frontend and Backend added successfully."}
 
 
@@ -207,7 +227,9 @@ def delete_to_file(haproxy_name):
     inside_backend = False
     inside_listen_stats = False
     backend_name = None
+    haproxy_count = 0  # Counter to check remaining HAProxy frontends
 
+    # First pass: Remove frontend and backend sections related to haproxy_name
     for line in lines:
         stripped_line = line.strip()
 
@@ -235,20 +257,36 @@ def delete_to_file(haproxy_name):
             backend_name = None  # Reset backend name after processing
             continue
         
-        # Handle listen stats section
-        if stripped_line.startswith("listen stats"):
-            inside_listen_stats = True
-            continue  # Skip this line (delete it)
-        
-        if inside_listen_stats and not stripped_line:
-            # End of listen stats block
-            inside_listen_stats = False
-            continue
+        # Count remaining HAProxy frontends
+        if stripped_line.startswith("frontend"):
+            haproxy_count += 1  # Count all remaining frontends except the one being deleted
 
         # Add non-empty lines and lines outside of the deleted sections
-        if not inside_frontend and not inside_backend and not inside_listen_stats:
-            if stripped_line or new_lines and new_lines[-1].strip():  # Avoid adding multiple blank lines
+        if not inside_frontend and not inside_backend:
+            if stripped_line or (new_lines and new_lines[-1].strip()):  # Avoid adding multiple blank lines
                 new_lines.append(line)
+
+    # Second pass: Remove listen stats section if only one haproxy_name is left
+    if haproxy_count == 0:
+        final_lines = []
+        inside_listen_stats = False
+        for line in new_lines:
+            stripped_line = line.strip()
+            
+            if stripped_line.startswith("listen stats"):
+                inside_listen_stats = True
+                continue  # Skip this line (delete it)
+            
+            if inside_listen_stats and not stripped_line:
+                # End of listen stats block
+                inside_listen_stats = False
+                continue
+
+            # Add non-empty lines and lines outside of the deleted sections
+            if not inside_listen_stats:
+                final_lines.append(line)
+        
+        new_lines = final_lines
 
     # Write the updated configuration back to the file
     with open(CONFIG_FILE_PATH, 'w') as haproxy_cfg:
@@ -259,23 +297,22 @@ def delete_to_file(haproxy_name):
         DOMAIN_FILE_PATH
     ]
 
+    # Only delete the files if they exist, do not return error
     for ssl in ssl_folder_content:
         if os.path.exists(ssl):
             os.remove(ssl)
-        else:
-            return 0
-        
+
     return f"Frontend '{haproxy_name}' and its associated backend have been deleted."
 
-
 def haproxy_info():
+    haproxy_data = []
     layer7_count = 0
     layer4_count = 0
-    haproxy_name = ''
     frontend_port = ''
     lb_method = ''
     backend_servers = []
     modes = set()
+    haproxy_name = ''
     inside_frontend = False
     inside_backend = False
 
@@ -287,40 +324,67 @@ def haproxy_info():
 
             # Detect frontend blocks
             if line.startswith('frontend '):
-                haproxy_name = line.split()[1]  # Capture the current frontend name
-                inside_frontend = True  # We are inside a frontend block
-                inside_backend = False  # We are inside a frontend block
+                # Jika sebelumnya sudah ada data frontend yang dikumpulkan, simpan dulu
+                if haproxy_name:
+                    haproxy_data.append({
+                        'haproxy_name': haproxy_name,
+                        'frontend_port': frontend_port,
+                        'lb_method': lb_method,
+                        'backend_servers': backend_servers,
+                        'modes': modes
+                    })
+
+                # Reset variabel untuk frontend baru
+                haproxy_name = line.split()[1]
+                frontend_port = ''
+                lb_method = ''
+                backend_servers = []
+                modes = set()
+                inside_frontend = True
+                inside_backend = False
 
             # Detect backend blocks
             elif line.startswith('backend '):
-                haproxy_name = line.split()[1]  # Capture the current backend name
-                inside_frontend = False  # We are no longer inside a frontend block
-                inside_backend = True  # We are no longer inside a frontend block
-            
-            if inside_frontend and line.startswith('bind '):
-                bind_parts = line.split()
-                if ':' in bind_parts[1]:  # Assuming bind format is "bind *:80"
-                    frontend_port = bind_parts[1].split(':')[1]
-            
-            if inside_backend and line.startswith('balance '):
-                lb_method = line.split()[1]
-            
-            if inside_backend and line.startswith('server '):
-                parts = line.split()
-                server_name = parts[1]  # Extract server name
-                server_ip = parts[2].split(':')[0]  # Extract server IP (before the colon)
-                backend_servers.append((server_name, server_ip))
+                inside_frontend = False
+                inside_backend = True
 
-            # Check for protocol modes only inside frontend blocks
-            if inside_frontend and line.startswith('mode'):
-                mode = line.split()[1]
-                modes.add(mode)
-                if mode == 'http':
-                    layer7_count += 1
-                elif mode == 'tcp':
-                    layer4_count += 1
+            # Handle frontend settings
+            if inside_frontend:
+                if line.startswith('bind '):
+                    bind_parts = line.split()
+                    if ':' in bind_parts[1]:  # Assuming bind format is "bind *:80"
+                        frontend_port = bind_parts[1].split(':')[1]
 
-    return frontend_port, layer7_count, layer4_count, haproxy_name, modes, lb_method, backend_servers
+                if line.startswith('mode'):
+                    mode = line.split()[1]
+                    modes.add(mode)
+                    if mode == 'http':
+                        layer7_count += 1
+                    elif mode == 'tcp':
+                        layer4_count += 1
+
+            # Handle backend settings
+            if inside_backend:
+                if line.startswith('balance '):
+                    lb_method = line.split()[1]
+
+                if line.startswith('server '):
+                    parts = line.split()
+                    server_name = parts[1]
+                    server_ip = parts[2].split(':')[0]
+                    backend_servers.append((server_name, server_ip))
+
+        # Pastikan data terakhir dimasukkan ke dalam list
+        if haproxy_name:
+            haproxy_data.append({
+                'haproxy_name': haproxy_name,
+                'frontend_port': frontend_port,
+                'lb_method': lb_method,
+                'backend_servers': backend_servers,
+                'modes': modes
+            })
+
+    return haproxy_data, layer7_count, layer4_count
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -364,11 +428,19 @@ def home():
     if not session.get('logged_in'):
         return '<script>alert("Harus login dulu"); window.location.href = "/";</script>'
 
-    frontend_port, layer7_count, layer4_count, haproxy_name, modes, lb_method, backend_servers = haproxy_info()
-    haproxy_exists = is_haproxy_exist()
+    # frontend_port, layer7_count, layer4_count, haproxy_name, modes, lb_method, backend_servers = haproxy_info()
+    haproxy_data, layer7_count, layer4_count = haproxy_info()
     domain_exists = is_domain_exist()
-
-    return render_template('home.html', frontend_port=frontend_port, layer7_count=layer7_count, layer4_count=layer4_count, haproxy_name=haproxy_name, modes=modes, lb_method=lb_method, backend_servers=backend_servers, haproxy_exists=haproxy_exists, domain_exists=domain_exists)
+    haproxy_exists = is_haproxy_exist()
+    return render_template(
+        'home.html', 
+        haproxy_data=haproxy_data, 
+        haproxy_exists=haproxy_exists, 
+        domain_exists=domain_exists,
+        layer7_count=layer7_count,
+        layer4_count=layer4_count
+    )
+    # return render_template('home.html', frontend_port=frontend_port, layer7_count=layer7_count, layer4_count=layer4_count, haproxy_name=haproxy_name, modes=modes, lb_method=lb_method, backend_servers=backend_servers, haproxy_exists=haproxy_exists, domain_exists=domain_exists)
 
 
 @app.route('/logout')
@@ -412,26 +484,23 @@ def add():
         ))
         
         # Save configuration to file
-        result = save_to_file(data)
+        save_to_file(data)
 
-        if result['success']:
-            if 'save_reload_create' in request.form:
-                # Run haproxy -c -V -f to check the configuration
-                check_result = subprocess.run(['haproxy', '-c', '-V', '-f', CONFIG_FILE_PATH], capture_output=True, text=True)
-                check_output = check_result.stdout
+        if 'save_reload_create' in request.form:
+            # Run haproxy -c -V -f to check the configuration
+            check_result = subprocess.run(['haproxy', '-c', '-V', '-f', CONFIG_FILE_PATH], capture_output=True, text=True)
+            check_output = check_result.stdout
 
-                # Check if there was an error, and if so, append it to the output
-                if check_result.returncode != 0:
-                    error_message = check_result.stderr
-                    check_output += f"\n\nError occurred:\n{error_message}"
-                else:
-                    # If no error, run haproxy -D -f to reload HAProxy
-                    reload_result = subprocess.run(['systemctl', 'restart', 'haproxy', CONFIG_FILE_PATH], capture_output=True, text=True)
-                    check_output += f"\n\nHAProxy Restart Output:\n{reload_result.stdout}"
+            # Check if there was an error, and if so, append it to the output
+            if check_result.returncode != 0:
+                error_message = check_result.stderr
+                check_output += f"\n\nError occurred:\n{error_message}"
+            else:
+                # If no error, run haproxy -D -f to reload HAProxy
+                reload_result = subprocess.run(['systemctl', 'restart', 'haproxy', CONFIG_FILE_PATH], capture_output=True, text=True)
+                check_output += f"\n\nHAProxy Restart Output:\n{reload_result.stdout}"
 
-            return redirect(url_for('home'))
-        else:
-            return '<script>alert("Konfigurasi HAProxy sudah ada. Tidak bisa menambah konfigurasi baru."); window.location.href = "/home";</script>'
+        return redirect(url_for('home'))
 
     return render_template('HAProxy/add.html')
 
@@ -444,7 +513,7 @@ def edit(haproxy_name):
     ssl_cert_content = ''
     domain_name_content = ''
 
-    data = read_from_file()
+    data = read_from_file(haproxy_name)
     item = next((d for d in data if d['haproxy_name'] == haproxy_name), None)
 
     if request.method == 'POST':
@@ -512,10 +581,9 @@ def edit(haproxy_name):
     return render_template('HAProxy/edit.html', **item, ssl_cert_path=ssl_cert_content, domain_name=domain_name_content)
 
 
-@app.route("/delete", methods=['POST'])
-def delete():
+@app.route("/delete/<string:haproxy_name>", methods=['POST'])
+def delete(haproxy_name):
     if request.method == 'POST':
-        haproxy_name = request.form['haproxy_name']
 
         delete_to_file(haproxy_name)  # Use custom function to save the updated data
 
